7c1475641d1a5f83cd831cc6ef26eff9c7889602
YaroslavLazorenko <lazorenko85@gmail.com>
2021-09-21 02:36:43 +0300

    Add files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { name } from "./app.js";
+import "./css/styles.css";
+
+alert("Webpack test");
+console.log(name);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "my-bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    port: 4444,
+    open: true,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
+};
